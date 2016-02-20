79f599e51469ec32ba900c2a5b5aa20bb926dc9f
Jonathan Nutzmann <jnutzmann@google.com>
2016-02-19 17:58:40 -0800

    Adding RTC files.

---
+++ src/rtc.c
@@ -0,0 +1,36 @@
+/********************************************************************
+rtc.c
+
+Copyright (c) 2015, Jonathan Nutzmann
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+********************************************************************/
+
+/****************************************************************************
+ * Includes
+ ***************************************************************************/
+
+#include "rtc.h"
+
+/****************************************************************************
+ * Private variables
+ ***************************************************************************/
+
+
+/****************************************************************************
+ * Public Functions
+ ***************************************************************************/
+
+void rtc_init ( void )
+{
+
+}
